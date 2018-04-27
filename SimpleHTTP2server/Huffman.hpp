0b306c049b93850aeba0f1de1638b23a8a0e2057
Trygve <trygve55@gmail.com>
2018-04-27 14:07:14 +0200

    Code cleanup

--- SimpleHTTP2server/Huffman.hpp
+++ SimpleHTTP2server/Huffman.hpp
@@ -24,73 +24,8 @@ private:
   
 public:
   static std::vector<HuffmanBitString> getHuffmanStrings();
-/*
-  //Returns the Huffman encoded bytes from inputData.
-  static uint8_t* encode(uint8_t inputData[], int length) {
-    int maxFreq = 0, max2Freq = 1, freqBits = 0, outIterator = 0, valueOut;
-    std::bitset<16> outData;
 
-    for(int i = 0; i < length; i++)
-      if(freqTable[i] > maxFreq)
-        maxFreq = freqTable[i];
-
-    while(maxFreq > max2Freq) {
-      freqBits++;
-      max2Freq = max2Freq << 1;
-    }
-
-    int freqBitsWrite = freqBits;
-    for(int i = 0; i < 8; i++) {
-      if(freqBitsWrite % 2 != 0) {
-        outData.set(outIterator);
-      }
-      outIterator++;
-      freqBitsWrite = freqBitsWrite >> 1;
-    }
-
-    for(int i = 0; i < length; i++) {
-      valueOut = freqTable[i];
-
-      for(int j = 0; j < freqBits; j++) {
-        if(valueOut % 2 == 1)
-          outData.set(outIterator);
-        outIterator++;
-        valueOut = valueOut >> 1;
-      }
-    }
-
-    std::vector<HuffmanBitString> huffmanBitStrings = getHuffmanStrings(getHuffmanTree());
-
-    HuffmanBitString* currentHuffmanBitString;
-    const int bit_length = 16;
-    std::bitset<bit_length> bitSet;
-
-    for(int i = 0; i < length; i++) {
-      currentHuffmanBitString = nullptr;
-
-      for(unsigned int j = 0; j < huffmanBitStrings.size(); j++) {
-        if(inputData[i] == huffmanBitStrings[j].getaByte()) {
-          currentHuffmanBitString = &huffmanBitStrings[j];
-          break;
-        }
-      }
-
-      bitSet = currentHuffmanBitString->getBitSet();
-
-      for(int j = 0; j < currentHuffmanBitString->length(); j++) {
-        if(bitSet[j])
-          outData.set(outIterator);
-        outIterator++;
-      }
-    }
-    uint8_t result[bit_length];
-    for (int i = 0; i < bit_length; i++) {result[i] = outData[i];}
-    return result;
-  }
-  
-  */
-
-  //Returns the decoded bytes from inputData.
+  //Inserts the decoded bytes from inputData to outputData.
   static void decode(char *inputData, unsigned int length, std::vector<char> &outputData);
 };
 
